85626547d6dc299e3c06969afa5f4aa48f9319f8
FelipePacheco30 <felipeferreirapacheco30@gmail.com>
2026-02-02 19:01:11 -0300

    chore: bootstrap Vue 3 application structure

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <header>
+      <h1>Operadoras de Planos de Saúde</h1>
+    </header>
+
+    <router-view />
+  </main>
+</template>
+
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+main {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 16px;
+}
+
+header {
+  margin-bottom: 24px;
+}
+</style>
