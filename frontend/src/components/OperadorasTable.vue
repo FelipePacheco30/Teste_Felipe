d69767e8abf43cc90c7e3a1a2ff7fb4eab8d099d
FelipePacheco30 <felipeferreirapacheco30@gmail.com>
2026-02-02 19:12:04 -0300

    feat: implement operadoras table component

---
+++ frontend/src/components/OperadorasTable.vue
@@ -0,0 +1,32 @@
+<template>
+  <table border="1" width="100%" cellspacing="0" cellpadding="8">
+    <thead>
+      <tr>
+        <th>CNPJ</th>
+        <th>Razão Social</th>
+        <th>UF</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="op in operadoras" :key="op.cnpj">
+        <td>
+          <router-link :to="`/operadoras/${op.cnpj}`">
+            {{ op.cnpj }}
+          </router-link>
+        </td>
+        <td>{{ op.razao_social }}</td>
+        <td>{{ op.uf }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+defineProps({
+  operadoras: {
+    type: Array,
+    required: true
+  }
+})
+</script>
