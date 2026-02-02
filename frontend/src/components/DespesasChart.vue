6425f8e0f60649cdee43c398c4aa665177c4719c
FelipePacheco30 <felipeferreirapacheco30@gmail.com>
2026-02-02 19:14:21 -0300

    feat: add despesas chart component

---
+++ frontend/src/components/DespesasChart.vue
@@ -0,0 +1,33 @@
+<template>
+  <canvas ref="canvas"></canvas>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import Chart from 'chart.js/auto'
+
+const props = defineProps({
+  dados: {
+    type: Array,
+    required: true
+  }
+})
+
+const canvas = ref(null)
+
+onMounted(() => {
+  new Chart(canvas.value, {
+    type: 'bar',
+    data: {
+      labels: props.dados.map(d => d.uf),
+      datasets: [
+        {
+          label: 'Total de Despesas',
+          data: props.dados.map(d => d.total),
+          backgroundColor: '#42b983'
+        }
+      ]
+    }
+  })
+})
+</script>
