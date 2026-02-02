a837d289b996b2fc7add6bdf05d33c3aaa10ea5d
FelipePacheco30 <felipeferreirapacheco30@gmail.com>
2026-02-02 19:15:23 -0300

    feat: add pagination component

---
+++ frontend/src/components/Pagination.vue
@@ -0,0 +1,29 @@
+<template>
+  <div style="margin-top: 16px">
+    <button
+      :disabled="page === 1"
+      @click="$emit('change', page - 1)"
+    >
+      Anterior
+    </button>
+
+    <span style="margin: 0 12px">
+      Página {{ page }}
+    </span>
+
+    <button
+      :disabled="page * limit >= total"
+      @click="$emit('change', page + 1)"
+    >
+      Próxima
+    </button>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  page: Number,
+  limit: Number,
+  total: Number
+})
+</script>
